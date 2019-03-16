a6b37cbe5ff090d72523024a9b18231ce163b662
smile0719 <smile190719@gmail.com>
2019-03-16 19:57:51 -0400

    fix the audio player position

--- src/containers/CardContainer/CardContainer.jsx
+++ src/containers/CardContainer/CardContainer.jsx
@@ -37,9 +37,9 @@ class CardContainer extends PureComponent {
           Object.keys(artists).map(key => 
                   <Card key={key} style={{backgroundColor:"#C0D6DF"}}>
                     <h5 onClick={() => this.showProfile(artists[key])} style={{textAlign:"center",color:"#000000",marginTop:"5%"}} >{artists[key]['artist']}</h5>
-                    <div onClick={() => this.showProfile(artists[key])} style={{height: "60%", overflow: "hidden"}}><img onClick={this.toggleProfile} style={{width:"100%",padding: "2% 2%"}} src={artists[key]['image']} /></div>
+                    <div onClick={() => this.showProfile(artists[key])} style={{height: "50%", overflow: "hidden"}}><img onClick={this.toggleProfile} style={{width:"100%",padding: "2% 2%"}} src={artists[key]['image']} /></div>
                     <Button style={{backgroundColor:"#D8315B",border:"none", marginLeft:"10%",width:"80%",marginTop:"5%",marginBottom:"1%"}}> Support This Artist</Button>
-                    <ReactAudioPlayer style={{marginTop:"1%"}}
+                    <ReactAudioPlayer style={{marginLeft:"12%",marginTop:"1%"}}
                       src={artists[key]['clip']}
                       controls
                     />
